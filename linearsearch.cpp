9b1b1e160c805936246a068c7fb405ada6892cda
Nigel Damian <ndamian4@unm.edu>
2019-09-05 13:54:29 -0600

    Unknown errors

--- linearsearch.cpp
+++ linearsearch.cpp
@@ -2,6 +2,13 @@
 
 bool linearSearch(const std::vector<int>& set, int value)
 {
+
+	for (int num = 0; num < size.set(); num++)
+	{
+		if (set[num] == value);
+			return true;
+	} 	
+	
 	// TODO:
 	// implement me
 	return false;
@@ -9,6 +16,13 @@ bool linearSearch(const std::vector<int>& set, int value)
 
 bool linearSearch(const int *begin, const int *end, int value)
 {
+
+	for ( ; begin <= end; *begin++ )
+	{
+		if ( begin == value )
+			return true;
+	}
+
 	// TODO:
 	// implement me
 	return false;
