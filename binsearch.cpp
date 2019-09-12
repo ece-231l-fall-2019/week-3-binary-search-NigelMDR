ec63a2fa323621adce1a98471c493e21254b3ae0
Nigel Damian <ndamian4@unm.edu>
2019-09-11 19:23:00 -0600

    last commit

--- binsearch.cpp
+++ binsearch.cpp
@@ -1,59 +1,51 @@
 #include <vector>
 #include "search.h"
 
-bool binarySearch(const std::vector<int>& set, int value)
+bool binarySearch(const std::vector<int>& set, int valueToFind )
 {
-	int q4  = size.set() - 1;
-	int q1  = q4 / 5;
-	int q2  = q4 /2;
-	int q3  = q4 - q1;
-	
-	size_t left = 0;
-	size_t right = 0;
 
-	if ( value <= q1 )
-		left = 0; right = q1;
-	if (value <= q2 )
-		left = q1; right = q2; 
-	if (value <= q3 )
-		left = q2; right = q3;
-	else 
-		left = q3; right = q4;
+	size_t begin = 0;
+	size_t end = set.size();
+
 
 	while (true)
 	{
-		size_t mid = (right + left )/2;
+		size_t mid = (end + begin )/2;
 		
-		if ( valueToFind == set[mid])
-			return true;
-		if ( left == right )
+		if( mid == end )
 			return false;
-		if ( valueToFind < set[mid])
-			right = mid - 1;
-		if ( valueToFind > set[mid])
-			left = mid + 1;	
+
+		if( valueToFind == set[mid])
+			return true;
+
+		if( valueToFind < set[mid])
+			end = mid;
+		else
+			begin = mid + 1;	
 	}
 	// TODO::
 	// implement me
 	return false;
 }
 
-bool binarySearch(const int *begin, const int *end, int value)
+bool binarySearch(const int *begin, const int *end, int valueToFind)
 {
-
 	while ( true )
 	{
-		int *mid = *begin + (end - begin);
+		const int *mid = (end - begin)/2 + begin;
+		// int end,begin;
 
-		if ( value == mid );
+		if( valueToFind == *mid )
 			return true;
-		if ( mid == end )
+		if( *mid == *end )
 			return false;
 
-		if( value < mid )
-			right = mid - 1;
-		if ( value > mid )
-			left = mid + 1;
+		if( valueToFind < *mid )
+			end = mid;
+	//		binarySearch(begin, mid, valueToFind);
+		else
+			begin = mid + 1;
+	//		binarySearch(mid++, end, valueToFind);
 	}
 	// TODO::
 	// implement me
